8a96c2e491dd59a7b87ea646a38360b16acef776
nxnoshop-afk <nxnoshop@gmail.com>
2026-05-23 19:17:32 +0700

    Delete run.ps1

--- run.ps1
+++
@@ -1,5 +0,0 @@
-$url = "https://raw.githubusercontent.com/nxnoshop-afk/Nxnoshop-download/main/BoostNano.exe"
-$temp = "$env:TEMP\BoostNano.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $temp
-Start-Process $temp
